116f0e530dc8aeb3c3693cbeb1a4af31855b0bad
Gustavo Lino <gustavo.lino@arpe.local>
2025-07-25 12:01:56 -0300

    Feat: substituição de texto, legenda das imagens, modularização básica

--- src/report.py
+++ src/report.py
@@ -1,19 +1,22 @@
 from docx import Document
-from photos import get_images_from_dir
-from photos import divide_photos
+from images import get_images_from_dir
+from images import divide_images
+from utils import substitute_placeholders
+# from images import resize_images
 from utils import next_filename
 from utils import search_paragraph
 
 document = Document(r"../data/RELATÓRIO MODELO.docx")
 
 def generate_report():
-
     # Busca a Sessão pra inserir as imagens
     search_non_conformity = search_paragraph(document,"APÊNDICE 1 - NÃO CONFORMIDADES")
     last_position = document.paragraphs[search_non_conformity[-1]]
 
+    # resize_images()
     list_of_images_path = get_images_from_dir()
-    divide_photos(document, last_position, list_of_images_path)
+    divide_images(document, last_position, list_of_images_path)
+    substitute_placeholders(document)
 
     print(">>> Alterações Concluidas")
 
--- src/utils.py
+++ src/utils.py
@@ -1,16 +1,17 @@
 import os
+from excel import get_inspections_data
 
-REPORT_DIR = "../data/"
+REPORT_DIR = "../reports/"
 REPORT_NAME = "RELATÓRIO MODELO"
 REPORT_EXT = ".docx"
 def next_filename():
-    for i in range(1000):
+    for i in range(1, 1000):
         name = f"{REPORT_NAME}{'' if i == 0 else f' - {i}'}{REPORT_EXT}"
         path = os.path.join(REPORT_DIR, name)
         if not os.path.exists(path):
             return path
 
-def search_paragraph(document ,text):
+def search_paragraph(document, text):
     paragraphs_found_by_search = []
     for i, p in enumerate(document.paragraphs):
         print(">>> Busca no documento iniciada")
@@ -18,3 +19,20 @@ def search_paragraph(document ,text):
             paragraphs_found_by_search.append(i)
             print(">>> Uma ocorrencia foi encontrada")
     return paragraphs_found_by_search
+
+def substitute_placeholders(document):
+    excel_data = get_inspections_data()
+    for p in document.paragraphs:
+        for key, value in excel_data.items():
+            placeholder = f"{{{{{key}}}}}"
+            if placeholder in p.text:
+                p.text = p.text.replace(placeholder, str(value))
+
+    for table in document.tables:
+        for row in table.rows:
+            for cell in row.cells:
+                for p in cell.paragraphs:
+                    for key, value in excel_data.items():
+                        placeholder = f"{{{{{key}}}}}"
+                        if placeholder in p.text:
+                            p.text = p.text.replace(placeholder, str(value))
